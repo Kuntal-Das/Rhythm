b50b37b7ba7cd717c5e36f0199053aa3691acc03
Kuntal Das <25ckuntaldas@gmail.com>
2021-05-17 22:47:53 +0530

    Header Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from "react"
+import "../styles/header.scss"
 
-import logo from "../logo/big-light.svg"
+// import { ReactComponent as LogoDark } from "../logo/big-dark.svg"
+import { ReactComponent as LogoLight } from "../logo/big-light.svg"
 
-const Header = () => (
-    <header>
-        <div className="container">
-            {logo}
-        </div>
-    </header>
-)
+// import { ReactComponent as LogoDark } from "../logo/small-dark.svg"
+// import { ReactComponent as LogoLight } from "../logo/small-light.svg"
+import { ReactComponent as GithubIcon } from "../social-icons/github.svg"
+import { ReactComponent as TwitterIcon } from "../social-icons/twitter.svg"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container flex-spacebetween">
+                <a href="/"><LogoLight className="logo" /></a>
+                <div>
+                    <a className="icon-link" href="https://github.com/Kuntal-Das/Rhythm"><GithubIcon className="icon" /></a>
+                    <a className="icon-link" href="https://twitter.com/Kuntal449"><TwitterIcon className="icon" /></a>
+                </div>
+            </div>
+        </header>
+    )
+}
 
 export default Header
