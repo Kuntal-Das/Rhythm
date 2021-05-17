840ce902934a264c044ef72fc14547231c454374
Kuntal Das <25ckuntaldas@gmail.com>
2021-05-18 02:31:32 +0530

    footer basic styling

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,32 +1,33 @@
-import React from "react"
-import PropTypes from "prop-types"
-
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/slider.scss";
 
 const Slider = (props) => (
-	<div className="slider">
-    <span>{props.name}</span>
-    <input 
+  <div className="slider">
+    <span className="name">{props.name}</span>
+    <input
       type="range"
+      className="slider-input"
       name={props.name}
       min={props.min}
       step={props.step}
       // value={props.value}
     />
-    <span className="reading"></span>
+    <span className="reading">{props.value}</span>
   </div>
-)
+);
 Slider.propTypes = {
-  name : PropTypes.string,
+  name: PropTypes.string,
   max: PropTypes.number,
   min: PropTypes.number,
   value: PropTypes.number,
-  step: PropTypes.number,
-}
+  step: PropTypes.number
+};
 
 Slider.defaultProps = {
-  min:0,
-  max:100,
-  value:50,
-  step:5
-}
-export default Slider
+  min: 0,
+  max: 100,
+  value: 50,
+  step: 5
+};
+export default Slider;
